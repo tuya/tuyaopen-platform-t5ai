be1bc275b4019126b1c82515afbb517a509f5575
zhouss@tuya.com <zhouss@tuya.com>
2025-08-06 17:40:39 +0800

    replace asr lib with libtutuClear.a

--- tuyaos/tuyaos_adapter/src/driver/tkl_asr.c
+++ tuyaos/tuyaos_adapter/src/driver/tkl_asr.c
@@ -6,107 +6,81 @@
 
 #include "tkl_asr.h"
 
-#include "asr.h"
-#include "fst_01.h"
-#include "fst_02.h"
-#include "fst_types.h"
+#include "tutuClear.h"
+#include <os/mem.h>
 
-#include <driver/uart.h>
 
 /***********************************************************
 ************************macro define************************
 ***********************************************************/
-#define TKL_ASR_DETECT_CHUNK_SIZE (960)
+#define __EN_EXTERNALLY_ALLOCATION__ // enable this define for externally malloc TUTU objects
+#define __EN_DIAGNOSTICS__ //enable this to have the proper internal returned diagnostical information
+
+#define TKL_ASR_DETECT_CHUNK_SIZE (640)
 
 /***********************************************************
 ***********************typedef define***********************
 ***********************************************************/
-
+typedef struct {
+    W32   w32WakeWord;
+    char *asr_txt;
+    TKL_ASR_WAKEUP_WORD_E wakeup_word;
+} ASR_WAKEUP_WORD_MAP_T;
 
 /***********************************************************
 ***********************variable define**********************
 ***********************************************************/
-static Fst sg_wanson_fst;
+static void *sg_tutuclear_obj = NULL;
+static void *pExternallyAllocatedMem = NULL;
 static TKL_ASR_WAKEUP_WORD_E sg_wakeup_word_list[TKL_ASR_WAKEUP_WORD_MAX];
 static uint8_t sg_wakeup_word_cnt = 0;
+
+static ASR_WAKEUP_WORD_MAP_T cASR_WAKEUP_WORD_MAP[] = {
+    {1, "heytuya", TKL_ASR_WAKEUP_NIHAO_TUYA},
+    {2, "你好涂鸦", TKL_ASR_WAKEUP_NIHAO_TUYA},
+    {3, "小智同学", TKL_ASR_WAKEUP_XIAOZHI_TONGXUE},
+};
 
 /***********************************************************
 ***********************function define**********************
 ***********************************************************/
-static bool  __compare_wakeup_word(char *text, TKL_ASR_WAKEUP_WORD_E wakeup_word)
+static bool  __compare_wakeup_word(W32 w32WakeWord, TKL_ASR_WAKEUP_WORD_E wakeup_word)
 {
     bool ret = false;
+    ASR_WAKEUP_WORD_MAP_T *wakeup_info = NULL;
+    
+    for(int i = 0; i < sizeof(cASR_WAKEUP_WORD_MAP) / sizeof(ASR_WAKEUP_WORD_MAP_T); i++) {
+        if(cASR_WAKEUP_WORD_MAP[i].w32WakeWord == w32WakeWord) {
+            wakeup_info = &cASR_WAKEUP_WORD_MAP[i];
+            break;
+        }
+    }
 
-    if(NULL == text || strlen(text) == 0) {
+    if(NULL == wakeup_info) {
         return false;
     }
 
-    switch(wakeup_word) {
-        case TKL_ASR_WAKEUP_NIHAO_TUYA:
-            if (strcmp(text, "你好涂鸦") == 0   ||
-                strcmp(text, "嘿体优丫") == 0   ||
-                strcmp(text, "嗨体优丫") == 0   ||
-                strcmp(text, "嘿突丫") == 0     ||
-                strcmp(text, "嗨突丫") == 0) {  
+    bk_printf("TUTUClear_WakeWord -> %d %s\n", wakeup_info->w32WakeWord, wakeup_info->asr_txt);
 
-                ret = true;
-            }
-        break;
-        case TKL_ASR_WAKEUP_NIHAO_XIAOZHI:
-            if(strcmp(text, "你好小智") == 0) {
-                ret = true;
-            }
-        break;
-        case TKL_ASR_WAKEUP_XIAOZHI_TONGXUE:
-            if(strcmp(text, "小智同学") == 0) {
-                ret = true;
-            }
-        break;
-        case TKL_ASR_WAKEUP_XIAOZHI_GUANJIA:
-            if(strcmp(text, "小智管家") == 0) {
-                ret = true;
-            }
-        break;
-        case TKL_ASR_WAKEUP_XIAOAI_XIAOAI:
-            if(strcmp(text, "小艾小艾") == 0) {
-                ret = true;
-            }
-        break;
-        case TKL_ASR_WAKEUP_XIAODU_XIAODU:
-            if(strcmp(text, "小杜小杜") == 0) {
-                ret = true;
-            }
-        break;
-        default:
-            bk_printf("not support wakeup word:%d", wakeup_word);
-        break;
-    }
+    ret = (wakeup_word == wakeup_info->wakeup_word) ? true : false;
 
     return ret;
 }
 
 OPERATE_RET tkl_asr_init(void)
 {
-    if(Wanson_ASR_Init() < 0) {
-        bk_printf("Wanson_ASR_Init error\r\n");
+#ifdef __EN_EXTERNALLY_ALLOCATION__
+	int i = TUTUClear_QueryMemSz();
+    pExternallyAllocatedMem = os_malloc(i);
+	bk_printf("tutuClear DM usage = %d bytes, addr = %p\n", i, pExternallyAllocatedMem);
+#endif // __EN_EXTERNALLY_ALLOCATION__
+
+    W16 TUTUClear_ret = TUTUClear_Init(pExternallyAllocatedMem,
+						&sg_tutuclear_obj);
+    if (TUTUClear_ret != TUTU_OK) {
+        bk_printf("Fail to do TUTUClear_Init %d.\n", TUTUClear_ret);
         return OPRT_COM_ERROR;
     }
-
-    memset(&sg_wanson_fst, 0x00, sizeof(Fst));
-#if 1
-    sg_wanson_fst.states     = fst01_states;
-    sg_wanson_fst.num_states = fst01_num_states;
-    sg_wanson_fst.finals     = fst01_finals;
-    sg_wanson_fst.num_finals = fst01_num_finals;
-    sg_wanson_fst.words      = fst01_words;
-#else 
-    sg_wanson_fst.states     = fst02_states;
-    sg_wanson_fst.num_states = fst02_num_states;
-    sg_wanson_fst.finals     = fst02_finals;
-    sg_wanson_fst.num_finals = fst02_num_finals;
-    sg_wanson_fst.words      = fst02_words;
-#endif
-    Wanson_ASR_Set_Fst(&sg_wanson_fst);
 
     bk_printf("tkl_asr_init OK!\n");
 
@@ -132,10 +106,9 @@ uint32_t tkl_asr_get_process_uint_size(void)
 
 TKL_ASR_WAKEUP_WORD_E tkl_asr_recognize_wakeup_word(uint8_t *data, uint32_t len)
 {
-    uint8_t i = 0;
-    int ret = 0;
-    char *text;
-    float score;
+    int i = 0;
+    W32 w32WakeWord;
+    ASR_WAKEUP_WORD_MAP_T *wakeup_info = NULL;
 
     if(NULL == data || 0 == len) {
         bk_printf("param err\r\n");
@@ -152,19 +125,29 @@ TKL_ASR_WAKEUP_WORD_E tkl_asr_recognize_wakeup_word(uint8_t *data, uint32_t len)
         return TKL_ASR_WAKEUP_WORD_UNKNOWN;
     }
 
-    ret = Wanson_ASR_Recog((short*)data, len/2, (const char **)&text, &score);
-    if(ret != 1) {
-        if(ret == -1) {
-            bk_printf("Wanson_ASR_Recog error\r\n");
-        }
+    TUTUClear_OneFrame(sg_tutuclear_obj, (W16*)data, &w32WakeWord);
+    if (w32WakeWord == 0) {
         return TKL_ASR_WAKEUP_WORD_UNKNOWN;
     }
 
-    bk_printf("Wanson_ASR_Recog -> %s \n", text);
+    TUTUClear_Init(pExternallyAllocatedMem, &sg_tutuclear_obj);
 
-    for(i=0; i<sg_wakeup_word_cnt; i++) {
-        if(true ==__compare_wakeup_word(text, sg_wakeup_word_list[i])) {
-            return sg_wakeup_word_list[i];
+    for(i = 0; i < sizeof(cASR_WAKEUP_WORD_MAP) / sizeof(ASR_WAKEUP_WORD_MAP_T); i++) {
+        if(cASR_WAKEUP_WORD_MAP[i].w32WakeWord == w32WakeWord) {
+            wakeup_info = &cASR_WAKEUP_WORD_MAP[i];
+            break;
+        }
+    }
+
+    if(NULL == wakeup_info) {
+        return TKL_ASR_WAKEUP_WORD_UNKNOWN;
+    }
+
+    bk_printf("TUTUClear_WakeWord -> %d %s\n", wakeup_info->w32WakeWord, wakeup_info->asr_txt);
+
+    for(i = 0; i < sg_wakeup_word_cnt; i++) {
+        if(wakeup_info->wakeup_word == sg_wakeup_word_list[i]) {
+            return wakeup_info->wakeup_word;
         }
     }
 
@@ -173,7 +156,7 @@ TKL_ASR_WAKEUP_WORD_E tkl_asr_recognize_wakeup_word(uint8_t *data, uint32_t len)
 
 OPERATE_RET tkl_asr_deinit(void)
 {
-    Wanson_ASR_Release();
+    TUTUClear_Release(&sg_tutuclear_obj);
 
     memset(sg_wakeup_word_list, 0x00, sizeof(sg_wakeup_word_list));
     sg_wakeup_word_cnt = 0;
